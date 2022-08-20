72e2f02b2e3bda2e7669c987bbc7a2959b392dc5
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2022-08-20 00:12:20 -0300

    feat: some tests

--- db/migrate/20220819130608_create_coins.rb
+++ db/migrate/20220819130608_create_coins.rb
@@ -2,7 +2,6 @@ class CreateCoins < ActiveRecord::Migration[7.0]
   def change
     create_table :coins do |t|
       t.string :description
-      t.string :string
       t.string :acronym
       t.string :url_image
 
