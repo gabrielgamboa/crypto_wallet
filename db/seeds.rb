a7236764b99c574d66cd8cd4cd2b428dc1186072
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2022-09-26 20:04:25 -0300

    feat: aprendendo sobre a task db:seed do rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cadastrando moedas"
+
+Coin.create!(
+   description: "Bitcoin",
+   acronym: "BTC",
+   url_image:"https://upload.wikimedia.org/wikipedia/commons/c/cf/Bitcoin.com_logo.png"
+)
+
+Coin.create!(
+    description: "Etherium",
+    acronym: "ETH",
+    url_image:"https://media.moneytimes.com.br/uploads/2022/03/ethereum-pexels.jpg"
+ )
+
+
+puts "Moedas cadastradas com sucesso!"
